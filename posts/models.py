5aaad035635ef9f29e05c6ba4c956292ad014606
blava <misha@mblavatsky.zeg>
2018-04-24 14:05:31 +0400

    added Slug field and pre_save method in model

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from unidecode import unidecode
+from django.utils.text import slugify
 
-# Create your models here.
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
     title=models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, default=None)
+    image=models.ImageField(upload_to=upload_location,null=True,blank=True, width_field="width_field", height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     content = models.TextField()
     timestamp=models.DateTimeField(auto_now=False, auto_now_add=True)
     updated=models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -15,3 +23,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering=["-timestamp", "-updated"]
+
+@receiver(pre_save,sender=Post)
+def my_callback(sender, instance, *args, **kwargs):
+    instance.slug =slugify(instance.title, True)
--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    queryset = Post.objects.all().order_by('-updated')
+    queryset_list = Post.objects.all()
+
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+    page = request.GET.get('page')
+    queryset = paginator.get_page(page)
+
     return render(request, 'post_list.html', {'posts' : queryset})
 
 def post_detail(request, id):
@@ -19,7 +24,7 @@ def post_detail(request, id):
 
 def post_create(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Successfully created")
@@ -34,15 +39,16 @@ def post_create(request):
     return render(request, 'post_form.html', context)
 
 def post_update(request, id):
-    instance = get_object_or_404(Post, id=id)
+
     if request.method == "POST":
-        form = PostForm(request.POST, instance= instance)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
         messages.success(request, "Successfully Updated")
         return redirect("posts:homepage")
     else:
-        form  = PostForm(request.POST or None, instance=instance)
+        instance = get_object_or_404(Post, id=id)
+        form  = PostForm(instance=instance)
         context = { 'title' :  instance.title,
                 "instance" : instance,
                 "form" : form,
